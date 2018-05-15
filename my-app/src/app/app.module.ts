739e2f02255ddcc937829f8f88cb1a54a8c49ef4
makshc2 <makshc2@gmail.com>
2018-05-15 17:36:00 +0300

    initial commit

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { PanelComponent } from './component/panel/panel.component';
+import { AddBookComponent } from './component/add-book/add-book.component';
+import { EditBookComponent } from './component/edit-book/edit-book.component';
+import { AboutComponent } from './component/about/about.component';
+import { NotFoundComponent } from './component/not-found/not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { BooksService } from './services/books.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent,
+    AddBookComponent,
+    EditBookComponent,
+    AboutComponent,
+    NotFoundComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [BooksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
